d2d79d3464fc9a7e0e9df74fc12fad4ab6c8dacd
auduongtansang <1712145@student.hcmus.edu.vn>
2020-06-01 08:09:20 +0700

    Cleaning code

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 window.onload = function () {
+
+    //Get all necessary HTML elements
     var canvas1 = document.getElementById("myCanvas1");
     var context1 = canvas1.getContext("2d");
 
@@ -7,10 +9,12 @@ window.onload = function () {
 
     var video = document.getElementById("myVideo");
     var slider = document.getElementById("intensity");
-    var button = this.document.getElementById("myButton");
+    var button = document.getElementById("myButton");
 
+    //Threshold for edge detection
     var threshold = 250;
 
+    //Play/Pause button event handler
     button.onclick = function () {
         if (video.paused) {
             video.play();
@@ -22,10 +26,12 @@ window.onload = function () {
         }
     };
 
+    //Intensity slider event handler (threshold = MAX - intensity)
     slider.oninput = function () {
         threshold = 500 - parseInt(slider.value);
     };
 
+    //Do some initializations when video is ready
     video.oncanplay = function () {
         var vid = this;
 
@@ -35,29 +41,38 @@ window.onload = function () {
         button.disabled = false;
     };
 
+    //Extract video frames and detect edge while video is playing
     video.onplay = function () {
         var vid = this;
 
         (function loop() {
             if (!vid.paused && !vid.ended) {
+
+                //Draw original current frame on context1
                 context1.drawImage(vid, 0, 0);
 
+                //Get image data from context1 and detect edge
                 var frameData = context1.getImageData(0, 0, vid.videoWidth, vid.videoHeight);
                 var frameEdge = sobel(frameData, threshold);
 
+                //Draw edge image data on context2
                 context2.putImageData(frameEdge, 0, 0);
 
+                //Loop these things every 1000/30 miliseconds (30 fps)
                 setTimeout(loop, 1000 / 30);
             }
         })();
     };
 
+    //Change button to "Play" when video has ended
     video.onended = function () {
         button.innerHTML = "Play";
     };
 };
 
 function sobel(imgData, th) {
+
+    //Some image information
     var row = imgData.height;
     var col = imgData.width;
 
@@ -68,9 +83,11 @@ function sobel(imgData, th) {
 
     var newImgData = new ImageData(col, row);
 
+    //Loop for each pixel
     for (var i = 1; i < row - 1; i += 1)
         for (var j = 1; j < col - 1; j += 1) {
 
+            //Get value of 8 neighbor pixels
             var topLeft = data[(i - 1) * rowStep + (j - 1) * colStep + 1];
             var top = data[(i - 1) * rowStep + j * colStep + 1];
             var topRight = data[(i - 1) * rowStep + (j + 1) * colStep + 1];
@@ -80,22 +97,19 @@ function sobel(imgData, th) {
             var bottomLeft = data[(i + 1) * rowStep + (j - 1) * colStep + 1];
             var left = data[i * rowStep + (j - 1) * colStep + 1];
 
+            //Calculate the gradient
             var dx = (topRight - topLeft) + 2 * (right - left) + (bottomRight - bottomLeft);
             var dy = (bottomLeft - topLeft) + 2 * (bottom - top) + (bottomRight - topRight);
             var grad = Math.sqrt(dx * dx + dy * dy);
 
-            if (grad >= th) {
-                newImgData.data[i * rowStep + j * colStep] = 255;
-                newImgData.data[i * rowStep + j * colStep + 1] = 255;
-                newImgData.data[i * rowStep + j * colStep + 2] = 255;
-                newImgData.data[i * rowStep + j * colStep + 3] = 255;
-            }
-            else {
-                newImgData.data[i * rowStep + j * colStep] = 0;
-                newImgData.data[i * rowStep + j * colStep + 1] = 0;
-                newImgData.data[i * rowStep + j * colStep + 2] = 0;
-                newImgData.data[i * rowStep + j * colStep + 3] = 255;
-            }
+            //Thresholding
+            var center = i * rowStep + j * colStep;
+            newImgData.data[center + 3] = 255;
+
+            if (grad >= th)
+                newImgData.data[center] = newImgData.data[center + 1] = newImgData.data[center + 2] = 255;
+            else
+                newImgData.data[center] = newImgData.data[center + 1] = newImgData.data[center + 2] = 0;
         }
 
     return newImgData;
